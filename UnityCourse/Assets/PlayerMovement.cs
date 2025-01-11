4819bb4672de09021b4ea1e878927dbdff94f5a4
AndrewZ <andrewzemetskiy@gmail.com>
2025-01-11 19:22:59 +0300

    with direction animation

--- UnityCourse/Assets/PlayerMovement.cs
+++ UnityCourse/Assets/PlayerMovement.cs
@@ -1,16 +1,19 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Animator _playerAnimator;
     public float moveSpeed = 1f;
 
     private InputSystem _inputSystem;
     private Rigidbody2D _rb;
     private Vector2 _inputDirection;
-
-
+    private Vector2 _previousDirection;
+    private float _animDir = -1;
+    
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -19,29 +22,55 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnEnable()
     {
-        _inputSystem.Player.Move.performed += MoveTest;
-        _inputSystem.Player.Move.canceled += ctx => _inputDirection = Vector2.zero;
+        _inputSystem.Player.Move.performed += Move;
+        _inputSystem.Player.Move.canceled += ResetMovement;
         _inputSystem.Enable();
     }
 
     private void OnDisable()
     {
         _inputSystem.Disable();
-        _inputSystem.Player.Move.performed -= MoveTest;
+        _inputSystem.Player.Move.performed -= Move;
+        _inputSystem.Player.Move.canceled -= ResetMovement;
     }
     
-    private void MoveTest(InputAction.CallbackContext obj)
+    private void Move(InputAction.CallbackContext obj)
     {
-        Debug.Log($"Value: {obj.ReadValue<Vector2>()}");
         Vector2 input = obj.ReadValue<Vector2>();
+        float inputY = input.y;
+        float inputX = input.x;
 
-        if (input.y != 0) //vertical (W,S)
+        if (inputY != 0) //vertical (W,S)
         {
-            _inputDirection = new Vector2(1 * input.y, 0.5f * input.y);
+            _inputDirection = new Vector2(1 * inputY, 0.5f * inputY);
+            float newAnimDir= inputY > 0 ? 0 : 0.5f;
+            UpdateAnimationState(newAnimDir);
         }
-        else if (input.x != 0) //horizontal (A,D)
+        else if (inputX != 0) //horizontal (A,D)
         {
-            _inputDirection = new Vector2(1 * input.x, -0.5f * input.x);
+            _inputDirection = new Vector2(1 * inputX, -0.5f * inputX);
+            float newAnimDir = inputX > 0 ? 0.25f : 0.75f;
+            UpdateAnimationState(newAnimDir);
+        }
+    }
+    
+    private void UpdateAnimationState(float newAnimDir)
+    {
+        if (_animDir != newAnimDir) // Меняем анимацию только при изменении направления
+        {
+            _animDir = newAnimDir;
+            _playerAnimator.SetTrigger("Run");
+            _playerAnimator.SetFloat("Direction", _animDir);
+        }
+    }
+    
+    private void ResetMovement(InputAction.CallbackContext obj)
+    {
+        _inputDirection = Vector2.zero;
+        if (_animDir != -1)
+        {
+            _playerAnimator.SetTrigger("Idle");
+            _animDir = -1;
         }
     }
     
