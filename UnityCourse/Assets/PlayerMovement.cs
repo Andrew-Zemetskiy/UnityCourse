7a1bfb145c290660cadfec50181251f68bc9405e
AndrewZ <andrewzemetskiy@gmail.com>
2025-01-11 15:46:45 +0300

    movement by input system

--- UnityCourse/Assets/PlayerMovement.cs
+++ UnityCourse/Assets/PlayerMovement.cs
@@ -1,37 +1,55 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 1f;
 
-    private Rigidbody2D rb;
+    private InputSystem _inputSystem;
+    private Rigidbody2D _rb;
+    private Vector2 _inputDirection;
+
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _inputSystem = new InputSystem();
     }
 
+    private void OnEnable()
+    {
+        _inputSystem.Player.Move.performed += MoveTest;
+        _inputSystem.Player.Move.canceled += ctx => _inputDirection = Vector2.zero;
+        _inputSystem.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputSystem.Disable();
+        _inputSystem.Player.Move.performed -= MoveTest;
+    }
+    
+    private void MoveTest(InputAction.CallbackContext obj)
+    {
+        Debug.Log($"Value: {obj.ReadValue<Vector2>()}");
+        Vector2 input = obj.ReadValue<Vector2>();
+
+        if (input.y != 0) //vertical (W,S)
+        {
+            _inputDirection = new Vector2(1 * input.y, 0.5f * input.y);
+        }
+        else if (input.x != 0) //horizontal (A,D)
+        {
+            _inputDirection = new Vector2(1 * input.x, -0.5f * input.x);
+        }
+    }
+    
     private void FixedUpdate()
     {
-        Vector2 currentPos = rb.position;
+        Vector2 movement = _inputDirection * moveSpeed;
+        Vector2 newPos = _rb.position + movement * Time.fixedDeltaTime;
 
-        // Получаем направление движения (только одна кнопка может быть нажата)
-        Vector2 inputDirection = Vector2.zero;
-        if (Input.GetKey(KeyCode.W)) // Вверх (вправо-вверх)
-            inputDirection = new Vector2(1, 0.5f);
-        else if (Input.GetKey(KeyCode.S)) // Вниз (влево-вниз)
-            inputDirection = new Vector2(-1, -0.5f);
-        else if (Input.GetKey(KeyCode.A)) // Влево (влево-вверх)
-            inputDirection = new Vector2(-1, 0.5f);
-        else if (Input.GetKey(KeyCode.D)) // Вправо (вправо-вниз)
-            inputDirection = new Vector2(1, -0.5f);
-
-        // Вычисляем движение
-        Vector2 movement = inputDirection * moveSpeed;
-        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-
-        // Перемещаем персонажа
-        rb.MovePosition(newPos);
+        _rb.MovePosition(newPos);
     }
 }
