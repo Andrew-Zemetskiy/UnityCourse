9dc51e099ff0934bde33b743cacab9c242d94496
AndrewZ <andrewzemetskiy@gmail.com>
2025-01-12 19:12:35 +0300

    add slowZone

--- UnityCourse/Assets/Scripts/NavAgentMovement.cs
+++ UnityCourse/Assets/Scripts/NavAgentMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,14 +5,22 @@ public class NavAgentMovement : MonoBehaviour
 {
     private Camera _cam;
     private NavMeshAgent _agent;
+    private float _baseSpeed;
 
     private void Start()
     {
         _cam = Camera.main;
         _agent = GetComponent<NavMeshAgent>();
+        _baseSpeed = _agent.speed;
     }
 
-    void Update()
+    private void OnEnable()
+    {
+        ZoneManager.Instance.OnPlayerEnterSlowZone += OnEnterInSlowZone;
+        ZoneManager.Instance.OnPlayerExitSlowZone += ResetSpeed;
+    }
+
+    private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -23,5 +30,15 @@ public class NavAgentMovement : MonoBehaviour
                 _agent.SetDestination(hit.point);
             }
         }
+    }
+
+    private void OnEnterInSlowZone(float slowingStrength)
+    {
+        _agent.speed = _baseSpeed * (slowingStrength / 100);
+    }
+
+    private void ResetSpeed()
+    {
+        _agent.speed = _baseSpeed;
     }
 }
---
+++ UnityCourse/Assets/ZoneManager.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZoneManager : MonoBehaviour
+{
+    public static ZoneManager Instance;
+    public Action<float> OnPlayerEnterSlowZone;
+    public Action OnPlayerExitSlowZone;
+
+
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField, Range(0, 100)] private float _slowZoneStrength;
+
+
+    private bool _isInSlowZone = false;
+
+    private void Awake()
+    {
+        Instance = this;
+        if (_agent == null)
+        {
+            _agent = FindFirstObjectByType<NavMeshAgent>();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        SlowZoneCheck();
+    }
+
+    private void SlowZoneCheck()
+    {
+        bool currentlyInSlowZone = IsInSlowZone();
+
+        if (currentlyInSlowZone && !_isInSlowZone)
+        {
+            _isInSlowZone = true;
+            OnPlayerEnterSlowZone?.Invoke(_slowZoneStrength);
+            Debug.Log("Персонаж находится в зоне замедления!");
+        }
+        else if (!currentlyInSlowZone && _isInSlowZone)
+        {
+            _isInSlowZone = false;
+            OnPlayerExitSlowZone?.Invoke();
+            Debug.Log("Персонаж вне зоны замедления!");
+        }
+    }
+
+
+    private bool IsInSlowZone()
+    {
+        if (!_agent) return false;
+
+        NavMeshHit hit;
+
+        if (NavMesh.SamplePosition(_agent.transform.position, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            int areaMask = hit.mask;
+            int slowZoneMask = 1 << NavMesh.GetAreaFromName("SlowZone");
+
+            return (areaMask & slowZoneMask) != 0;
+        }
+
+        return false;
+    }
+}
